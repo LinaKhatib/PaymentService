55b7de937a33d391cdc480f22b2019a77ddbe9b9
Lina <hatiblina1@gmail.com>
2026-08-13 19:44:20 +0300

    feat: add domain models and entities

---
+++ src/TransactionService/Models/Operation.cs
@@ -0,0 +1,13 @@
+﻿using TransactionService.Models.Enums;
+
+namespace TransactionService.Models;
+
+public class Operation
+{
+    public string OperationId { get; set; } = string.Empty;
+    public string Amount { get; set; } = string.Empty;
+    public string Currency { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public OperationStatus Status { get; set; } = OperationStatus.CREATED;
+    public string? ProviderPaymentId { get; set; } = null;
+}
